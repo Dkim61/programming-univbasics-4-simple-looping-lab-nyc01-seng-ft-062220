13986937ab8455d508e2e9674c31a2e315ca48c2
david <dkim187@binghamton.edu>
2020-06-15 23:16:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,6 +13,9 @@ def  loop_message_n_times(string, num)
   count = 0
     while count <= num do
       puts string
-      count+= 1
+      count += 1
     end
 end
+
+def output_array(array)
+  count = 0
