2ac46b91c95114f84ec7a3cf0af1e1c920ecc217
david <dkim187@binghamton.edu>
2020-06-15 23:11:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,14 +3,16 @@
 
 def loop_message_five_times(string)
   count = 0
-  while count < 6 do
-  puts string
-  count += 1
-  end
+    while count < 6 do
+      puts string
+      count += 1
+    end
 end
 
 def  loop_message_n_times(string, num)
   count = 0
-  while count <= num do
-    puts string
-    count+=
+    while count <= num do
+      puts string
+      count+= 1
+    end
+end
